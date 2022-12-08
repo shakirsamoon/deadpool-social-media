0d8c7a0d374a8f359441ca6778ad3c342751bac5
Shakir Samoon <shakirsamoon@gmail.com>
2022-12-08 22:34:17 +0530

    build error fixed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Meta from "@components/Meta";
 import Link from 'next/link';
-import styles from '@styles/NotFound/NotFound.module.css';
+import styles from '@styles/notfound/NotFound.module.css';
 
 /**
  *  Layout for the 404 Not Found page
